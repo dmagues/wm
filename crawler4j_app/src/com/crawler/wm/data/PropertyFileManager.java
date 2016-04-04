e90662a99e20765da439c2072d8abb5eeeabbd87
Daniel Magues <dmagues@hotmail.com>
2016-04-04 20:16:20 +0200

    adding properties and Property manager

---
+++ crawler4j_app/src/com/crawler/wm/data/PropertyFileManager.java
@@ -0,0 +1,35 @@
+package com.crawler.wm.data;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PropertyFileManager {
+	private static final String PROPERTYFILE="com/crawler/wm/data/conf.properties";
+	private static Properties prop=null;
+	private static final InputStream input = PropertyFileManager.class.getClassLoader().getResourceAsStream(PROPERTYFILE);
+	
+	public static void readPropertyFile() throws IOException
+	{
+		prop = new Properties();		
+		prop.load(input);
+		
+	}
+	
+	public static List<String> getPropertiesValues(String mode, String group)
+	{
+		String strKey;
+		List<String> items = new ArrayList<String>();
+		for (Object key: prop.keySet())
+		{
+			strKey = (String)key;
+			if (strKey.contains(mode+"."+group))
+				items.add(prop.getProperty(strKey));		
+			
+		}
+		return items;
+	}
+
+}
